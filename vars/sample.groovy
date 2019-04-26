d6db496c09acb3deea583b04401c7fef062a4516
sudhakar13j <49972706+sudhakar13j@users.noreply.github.com>
2019-04-26 04:16:39 -0700

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,20 +1,6 @@
-//def call(from, to, subject)
-//{
-//println "Welcome. mail"
-//mail bcc: '', body: 'Demo', cc: from, from: from , replyTo: '', subject: subject, to: to
-//println "email sent successfully"
-//}
-
-class Example { 
-   static void main(String[] args) { 
-      //Initializing 2 variables 
-      int x = 5; 
-      int X = 6; 
-	  
-      //Printing the value of the variables to the console 
-      println("The value of x is " + x + "The value of X is " + X);  
-     //println "Welcome. mail"
-//mail bcc: '', body: 'Demo', cc: from, from: from , replyTo: '', subject: subject, to: to
-//println "email sent successfully"
-   }
+def call(from, to, subject)
+{
+println "Welcome. mail"
+mail bcc: '', body: 'Demo', cc: from, from: from , replyTo: '', subject: subject, to: to
+println "email sent successfully"
 }
