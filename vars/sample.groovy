39ec3c66d17e6595e4adfaf0b8070da4507250fa
sudhakar13j <49972706+sudhakar13j@users.noreply.github.com>
2019-04-26 02:40:59 -0700

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,14 +1,6 @@
-//def call(from, to, subject)
-//{
-//println "Welcome. mail"
-//mail bcc: '', body: 'Demo', cc: from, from: from , replyTo: '', subject: subject, to: to
-//println "email sent successfully"
-//}
-
-Class samp
+def call(from, to, subject)
 {
-  public static void main(string args[])
-  {
-    mail bcc: '', body: 'Demo', cc: from, from: from , replyTo: '', subject: subject, to: to
-  }
+println "Welcome. mail"
+mail bcc: '', body: 'Demo', cc: from, from: from , replyTo: '', subject: subject, to: to
+println "email sent successfully"
 }
