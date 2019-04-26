96a88be9dacb4375966290b5de38e9160632c8bc
sudhakar13j <49972706+sudhakar13j@users.noreply.github.com>
2019-04-25 23:49:05 -0700

    Create SendEmail.groovy

---
+++ src/Email/SendEmail.groovy
@@ -0,0 +1,5 @@
+node
+{
+mail bcc: '', body: 'Demo', cc: 'deepak.kumar@ravsoftsolutions.com', from: 'sudhakar.anandan@ravsoftsolutions.com', replyTo: '', subject: 'Demo Mail', to: 'sudhakar.anandan@ravsoftsolutions.com'
+echo 'email sent successfully'
+}
