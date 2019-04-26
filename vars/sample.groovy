f52a74d4a1341805f86f6306a27860a3e61fd610
sudhakar13j <49972706+sudhakar13j@users.noreply.github.com>
2019-04-26 05:38:28 -0700

    Update and rename sample.groovy to Example.groovy

--- vars/sample.groovy
+++
@@ -1,6 +0,0 @@
-def call(from, to, subject)
-{
-println "Welcome. mail"
-mail bcc: '', body: 'Demo', cc: from, from: from , replyTo: '', subject: subject, to: to
-println "email sent successfully"
-}
