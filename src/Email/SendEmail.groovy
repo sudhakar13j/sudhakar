b15bbc92c9306431e47ca5d266c0382f55802926
sudhakar13j <49972706+sudhakar13j@users.noreply.github.com>
2019-04-26 02:18:50 -0700

    Update SendEmail.groovy

--- src/Email/SendEmail.groovy
+++ src/Email/SendEmail.groovy
@@ -2,6 +2,6 @@ package Email
 def SendEmail(String from, String  to, String subject)
 {
 echo 'demo mail'
-mail bcc: '', body: 'Demo', cc: from, from: from , replyTo: '', subject: subject, to: to
+//mail bcc: '', body: 'Demo', cc: from, from: from , replyTo: '', subject: subject, to: to
 echo 'email sent successfully'
 }
